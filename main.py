dda9d80d18a8e6ca3c466b1a0e0f227c35e01aea
Nazariy Vesklyarov <nazariy.vesklyarov@gmail.com>
2025-07-26 14:51:33 +0300

    SolutionNV2

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def main() -> None:
         for skill_data in skills_data:
             Skill.objects.get_or_create(
                 name=skill_data["name"],
-                defaults={"bonus": skill_data["bonus"], "race": race}
+                race=race,
+                defaults={"bonus": skill_data["bonus"]}
             )
 
         guild = None
